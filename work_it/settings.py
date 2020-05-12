dc1eac823f58fa5cf6c88444c66dc3f10ef8503b
julie <julie@julies-MacBook-Pro.local>
2020-05-12 13:25:46 -0700

    update

--- work_it/settings.py
+++ work_it/settings.py
@@ -133,4 +133,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
